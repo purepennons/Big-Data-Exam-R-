09d0366991ff0b27418db58cd39cceea341a533b
purepennons <y80811@gmail.com>
2014-11-17 03:13:14 +0800

    Let peak.R show top of 3 lowest avg_flow

--- peak.R
+++ peak.R
@@ -3,7 +3,7 @@ library(ggplot2)
 flowdata = read.table(file="flowdata.csv",  header=TRUE, sep=",")
 avg_flow_data = subset(flowdata, select = c(datetime, avg_flow, weekday))
 attach(avg_flow_data)
-avg_flow_data =avg_flow_data[avg_flow>-1,]
+avg_flow_data =avg_flow_data[avg_flow>-1&weekday<6,]
 
 ##	split time
 temptime <- read.table(textConnection(as.character(avg_flow_data$datetime)))
@@ -14,7 +14,4 @@ temp1 = aggregate(avg_flow_data[2:2], by=list(avg_flow_data$datetime), mean)
 ##	rename col names.
 colnames(temp1) <- c("datetime", "avg_flow")
 
-print("Max: ")
-print(temp1[temp1$avg_flow==max(temp1$avg_flow),])
-print("Min: ")
-print(temp1[temp1$avg_flow==min(temp1$avg_flow),])
+print(temp1[temp1$avg_flow<37,])
